9398d052d5f032ba4bdce21d36a9e52977d453d1
Demonju <jake.zammit.d57298@mcast.edu.mt>
2021-01-08 16:12:50 +0100

    Added Laser + Damage

--- CarGame/Assets/Scripts/Obstacle.cs
+++ CarGame/Assets/Scripts/Obstacle.cs
@@ -5,16 +5,45 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour
 {
     [SerializeField] float obstacleSpeed = 0.3f;
+    [SerializeField] float shotCounter;
 
-    // Start is called before the first frame update
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+
+    [SerializeField] float maxTimeBetweenShots = 3f;
+
+    [SerializeField] GameObject BulletPrefab;
+
+    [SerializeField] float BulletSpeed = 0.3f;
+
     void Start()
     {
-        
+        //generate a random number
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        // every frame reduce the amount of time for shot
+        shotCounter -= Time.deltaTime;
+
+        if (shotCounter <= 0f)
+        {
+            EnemyFire();
+            //reset ShotCounter
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void EnemyFire()
+    {
+        //spawn an enemy laser and Enemy Position
+        GameObject enemyLaser = Instantiate(BulletPrefab, transform.position, Quaternion.identity) as GameObject;
+        //shoot laser downwards: -enemyLaserSpeed
+        enemyLaser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -BulletSpeed);
     }
 }
